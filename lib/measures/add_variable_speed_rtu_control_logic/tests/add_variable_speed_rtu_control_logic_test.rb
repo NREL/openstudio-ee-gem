a79d4bf4fbafede4f37366c2487fc2b2e782cf92
David Goldwasser <david.goldwasser@nrel.gov>
2020-02-28 12:10:38 -0700

    adding another measure from OpenStudio-measures

---
+++ lib/measures/add_variable_speed_rtu_control_logic/tests/add_variable_speed_rtu_control_logic_test.rb
@@ -0,0 +1,34 @@
+require 'openstudio'
+require 'openstudio/ruleset/ShowRunnerOutput'
+require 'minitest/autorun'
+
+require_relative '../measure.rb'
+
+class AddVariableSpeedRTUControlLogic_Test < MiniTest::Unit::TestCase
+
+  def test_good_argument_values
+
+    # create an instance of the measure
+    measure = AddVariableSpeedRTUControlLogic.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Ruleset::OSRunner.new
+
+    # make an empty workspace
+    workspace = OpenStudio::Workspace.new("Draft".to_StrictnessLevel, "EnergyPlus".to_IddFileType)
+
+    # get arguments
+    arguments = measure.arguments(workspace)
+    argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
+
+    # run the measure
+    measure.run(workspace, runner, argument_map)
+    result = runner.result
+    assert_equal("Success", result.value.valueName)
+    
+    # save the workspace to output directory
+    output_file_path = OpenStudio::Path.new(File.dirname(__FILE__) + "/output/test_output.idf")
+    workspace.save(output_file_path,true)
+  end
+
+end
