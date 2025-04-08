0934dcb5187f26e0cc2ba48dc4f0ff27e752fb3a
1jhu1 <jhu1@nrel.gov>
2025-04-08 16:25:52 -0600

    Add arguments: api_key, epd_type; edit functions: parse_product_epd and parse_industrial_epd

--- lib/measures/window_enhancement/resources/Test_API.py
+++ lib/measures/window_enhancement/resources/Test_API.py
@@ -1,62 +1,25 @@
 # Test EC3 API Call
 import requests
-import json
-import re
 import pprint
-'''
-EC3_category_array = {
-    "ConstructionMaterials": {
-        "Concrete": [
-            "ReadyMix",
-            "Shotcrete",
-            "CementGrout",
-            "FlowableFill",
-            "Precast",
-            "Rebar",
-            "WireMeshSteel",
-            "SCM",
-            "Cement",
-            "Aggregates"
-        ],
-        "Masonry": [
-            "Brick",
-            "ConcreteUnitMasonry",
-            "Mortar",
-            "Cementitious",
-            "Aggregates"
-        ],
-        "Steel": [],
-        "Aluminum": [],
-        "Wood": [],
-        "Sheathing": [],
-        "ThermalMoistureProtection": [], 
-        "Cladding": [],
-        "Openings": [], 
-        "Finishes": [],
-        "ConveyingEquipment": [],
-        "NetworkInfrastrucure": [], 
-        "Asphalt": [],
-        "Accessories": [], 
-        "ManufacturingInputs": [],
-        "BulkMaterials": [],
-        "Placeholders": []
-    },
-    "BuildingAssemblies": {
-        "NewCoustomAssembly": [],
-        "ReinforcedConcrete": [], 
-        "Walls": [],
-        "Floors": [], 
-        "GlazingFenesration": [], 
-    },
-}
-'''
+import os
+import configparser
 
-test_url = ("https://api.buildingtransparency.org/api/industry_epds?page_number=1&page_size=250&mf=!EC3%20search(%22WallPanels%22)%20WHERE%20%0A%20%20jurisdiction%3A%20IN(%22021%22)%20AND%0A%20%20epd__date_validity_ends%3A%20%3E%20%222025-03-20%22%20AND%0A%20%20epd_types%3A%20IN(%22Indsutry%20EPDs%22)%20!pragma%20eMF(%222.0%2F1%22)%2C%20lcia(%22TRACI%202.1%22)")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+repo_root = os.path.abspath(os.path.join(script_dir, "../../../.."))
+config_path = os.path.join(repo_root, "config.ini")
 
+if not os.path.exists(config_path):
+    raise FileNotFoundError(f"Config file not found: {config_path}")
+
+config = configparser.ConfigParser()
+config.read(config_path)
+API_TOKEN= config["EC3_API_TOKEN"]["API_TOKEN"]
+
+test_url = ("https://api.buildingtransparency.org/api/materials?page_number=1&page_size=25&mf=!EC3%20search(%22AluminiumExtrusions%22)%20WHERE%20%0A%20%20jurisdiction%3A%20IN(%22021%22)%20AND%0A%20%20epd__date_validity_ends%3A%20%3E%20%222025-04-08%22%20AND%0A%20%20epd_types%3A%20IN(%22Product%20EPDs%22)%20%0A!pragma%20eMF(%222.0%2F1%22)%2C%20lcia(%22TRACI%202.1%22)")
 # Headers for the request
 headers = {
     "Accept": "application/json",
-    "Authorization": "Bearer 5fk7wP4cJg6pcmx6ncZN0ftMdoVR8u"
+    "Authorization": "Bearer "+ API_TOKEN
 }
 
 # Execute the GET request
