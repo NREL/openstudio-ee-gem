1fa7bdca06c65c3e6739cbfa59654d114094f35a
1jhu1 <jhu1@nrel.gov>
2025-04-18 16:48:55 -0600

    Increase example model complexity; Reorganize EC3_lookup; Revise window frame calculations, assign embodied carbon to corresponding subsurface object

--- lib/measures/window_enhancement/resources/calculate_perimeter.py
+++ lib/measures/window_enhancement/resources/calculate_perimeter.py
@@ -1,18 +1,39 @@
 # save this function for wall construction
-def calculate_perimeter(self, sub_surface):
-    """Calculate the perimeter of the window from its vertices."""
+def calculate_geometry(self, sub_surface):
+    """
+    Calculate the length, width, perimeter, and area of the window from its vertices.
+    Assumes the window is a quadrilateral (typically a rectangle).
+    """
     vertices = sub_surface.vertices()
-    if len(vertices) < 2:
-        return 0.0
+    if len(vertices) != 4:
+        return {
+            "length": 0.0,
+            "width": 0.0,
+            "perimeter": 0.0,
+            "area": 0.0
+        }
 
+    # Calculate all edge lengths
+    edge_lengths = []
     perimeter = 0.0
-    num_vertices = len(vertices)
-
-    for i in range(num_vertices):
+    for i in range(4):
         v1 = vertices[i]
-        v2 = vertices[(i + 1) % num_vertices]  # Wrap around to the first vertex
-        edge_vector = v2 - v1  # Subtract two Point3d objects to get Vector3d
-        edge_length = edge_vector.length()  # Use length() to get the magnitude of the vector
-        perimeter += edge_length
+        v2 = vertices[(i + 1) % 4]
+        edge = v2 - v1
+        length = edge.length()
+        edge_lengths.append(length)
+        perimeter += length
 
-    return perimeter
+    # Assume opposite edges are equal, so we can group into two unique lengths
+    length = max(edge_lengths)
+    width = min(edge_lengths)
+
+    # Area = length × width
+    area = length * width
+
+    return {
+        "length": length,
+        "width": width,
+        "perimeter": perimeter,
+        "area": area
+    }
